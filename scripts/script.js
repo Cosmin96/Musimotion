6f93b328736ead55c2c5c2b43091fc9d869e6f91
Cosmin Bresug <marnicos2006@yahoo.com>
2018-01-21 00:04:13 +0000

    Added barz

--- scripts/script.js
+++ scripts/script.js
@@ -22,7 +22,7 @@ $(function() {
         var millisecondsPeriod = 5000;
 
         // Set the date we're counting down to
-        var countDownDate = new Date().getTime()+millisecondsPeriod;
+        var countDownDate = new Date().getTime();
 
         // Update the count down every 1 second
         var x = setInterval(function() {
